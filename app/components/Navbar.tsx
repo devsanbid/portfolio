fa1c664d99d47fee6942824febb602cec1285c54
devsanbid <devsanbid@gmail.com>
2026-03-10 18:06:44 +0545

    navbar fixed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -58,7 +58,7 @@ export default function Navbar() {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            SS.
+            SS
           </motion.a>
 
           {/* Desktop Navigation */}
